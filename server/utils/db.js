f08cc7b78cff9712be1d3bbbc2ec958c805c50cb
Marius Genton <mariusgenton@Mac.lan>
2025-06-10 17:39:37 -0700

    Try to fix Prisma init

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+console.log('Setting up Prisma', process.env.DATABASE_URL)
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL
+        }
+    }
+});
 
 export async function userExists(email) {
     if (!email) throw new Error('Missing email');
