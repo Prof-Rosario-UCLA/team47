ef5d6ae9e8ee83d9763c64602a64ccbfa741514a
Marius Genton <mariusgenton@Mac.lan>
2025-06-11 11:05:34 -0700

    Redis debugging

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -26,45 +26,43 @@ async function getRedisClient() {
 
 export async function fetchEventsFromCache(date) {
     const key = `events:${date}:all`;
-    return null;
 
-    // try {
-    //     const client = await getRedisClient();
+    try {
+        const client = await getRedisClient();
 
-    //     console.log(`Attempting to fetch events on ${date} from cache`);
-    //     const data = await client.get(key);
+        console.log(`Attempting to fetch events on ${date} from cache`);
+        const data = await client.get(key);
 
-    //     if (data && JSON.parse(data)) {
-    //         console.log('Cache hit');
-    //         return JSON.parse(data);
-    //     } else {
-    //         console.log('Cache miss');
-    //         return null;
-    //     }
+        if (data && JSON.parse(data)) {
+            console.log('Cache hit');
+            return JSON.parse(data);
+        } else {
+            console.log('Cache miss');
+            return null;
+        }
 
-    // } catch(e) {
-    //     console.error(`Error fetching from Redis:`, e);
-    //     return null;
-    // }
+    } catch(e) {
+        console.error(`Error fetching from Redis:`, e);
+        return null;
+    }
 }
 
 export async function cacheEvents(date, events, expiration = 300) {
     const key = `events:${date}:all`;
-    return true;
 
-    // try {
-    //     const client = await getRedisClient();
+    try {
+        const client = await getRedisClient();
 
-    //     console.log(`Writing events to cache for ${date}`);
-    //     await client.set(key, JSON.stringify(events), { EX: expiration });
-    //     console.log(`Cache updated.`);
+        console.log(`Writing events to cache for ${date}`);
+        await client.set(key, JSON.stringify(events), { EX: expiration });
+        console.log(`Cache updated.`);
 
-    //     return true;
+        return true;
 
-    // } catch(e) {
-    //     console.error(`Error caching to Redis:`, e);
-    //     return false;
-    // }
+    } catch(e) {
+        console.error(`Error caching to Redis:`, e);
+        return false;
+    }
 }
 
 export async function invalidateEventsCache(date) {
